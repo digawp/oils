6736492a502128674141e405c2e34984328e74cb
Eugene Yeo <yeo.eugene.oey@gmail.com>
2016-03-10 02:46:26 +0800

    resource chekcout

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 run:
 	python manage.py runserver 0.0.0.0:8000
 
+watch:
+	node_modules/.bin/webpack --watch
+
 shell:
 	python manage.py shell_plus
 
--- static/oils/assets/circulation/onestop/app.js
+++ static/oils/assets/circulation/onestop/app.js
@@ -15,6 +15,9 @@ const loggerMiddleware = createLogger();
 // Constant ActionTypes
 const LOOKUP_PATRON = 'LOOKUP_PATRON';
 const CHECKOUT_RESOURCE = 'CHECKOUT_RESOURCE';
+const FAIL_PATRON_LOOKUP = 'FAIL_PATRON_LOOKUP';
+const FAIL_CHECKOUT = 'FAIL_CHECKOUT';
+const SUCCESS_CHECKOUT = 'SUCCESS_CHECKOUT';
 const RETURN_RESOURCE = 'RETURN_RESOURCE';
 const RENEW_RESOURCE = 'RENEW_RESOURCE';
 const REQUEST_PATRON = 'REQUEST_PATRON';
@@ -27,21 +30,67 @@ let OneStopActions = {
     return (dispatch) => {
       dispatch(OneStopActions.requestPatron(patron));
       fetch(`/api/patrons/${patron}/`)
-        .then(response =>
-            response.json()
-            .then(json=>({json, response}))
-        )
+        .then(response =>{
+            return response.json().then(json=>({json, response}));
+        })
         .then(({json, response}) => {
           if (!response.ok){
-            Promise.reject(json);
+            dispatch(OneStopActions.failPatronLookup(json));
+          } else {
+            dispatch(OneStopActions.receivePatron(patron, json))
           }
-          dispatch(OneStopActions.receivePatron(patron, json))
         });
     };
   },
 
-  checkoutResource(identifier) {
-    return { type: CHECKOUT_RESOURCE, identifier };
+  failPatronLookup(json) {
+    return {
+      type: FAIL_PATRON_LOOKUP,
+      json,
+    };
+  },
+
+  selectLoanAction(loan, action) {
+    return {
+      type: '',
+    }
+  },
+
+  checkoutResource(patron, resource) {
+    return (dispatch) => {
+      dispatch(OneStopActions.requestCheckout(patron, resource));
+      fetch(`/api/loans/`, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          patron,
+          resource,
+        })})
+      .then(response=>response.json().then(json=>({json, response})))
+      .then(({json, response}) => {
+        if (!response.ok){
+          dispatch(OneStopActions.failCheckout(json));
+        } else {
+          dispatch(OneStopActions.successCheckout(json));
+          dispatch(OneStopActions.lookupPatron(patron));
+        }
+      });
+    };
+  },
+
+  requestCheckout(patron, resource) {
+    return { type: CHECKOUT_RESOURCE, patron, resource };
+  },
+
+  failCheckout(json) {
+    return { type: FAIL_CHECKOUT, json };
+  },
+
+  successCheckout(json) {
+    return { type: SUCCESS_CHECKOUT, json };
   },
 
   returnResource(identifier) {
@@ -109,8 +158,8 @@ const patronReducer = (state=initialPatron, action) => {
       });
     default:
       return Object.assign({}, state, {
-        profile: patronProfileReducer(undefined, action),
-        circulation: circulationReducer(undefined, action),
+        profile: patronProfileReducer(state.profile, action),
+        circulation: circulationReducer(state.circulation, action),
       });
   }
 };
@@ -136,9 +185,8 @@ class PatronLookupForm extends Component {
 
   handleSubmit(e) {
     const patron = e.target.value.trim();
-    if (e.key === 'Enter') {
+    if (e.key === 'Enter' && patron !== '') {
       this.props.onLookup(patron);
-      this.setState({ patron: '' });
     }
   }
 
@@ -184,23 +232,54 @@ class PatronPanel extends Component {
         <PatronLookupForm
           patronId={patronProfile.id}
           onLookup={onLookup} />
-        <PatronDetail
-          patronProfile={patronProfile} />
+        {
+          patronProfile.id ? (
+            <PatronDetail
+              patronProfile={patronProfile} />
+          ) : null
+        }
       </div>
+    );
+  }
+}
+
+class LoanActions extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      value: ''
+    };
+  }
+
+  handleChange(e) {
+    this.setState({value: e.target.value });
+    this.props.onSelect(this.props.loan, this.state.value);
+    this.setState({value: ''});
+  }
+
+  render() {
+    return (
+      <select onChange={this.handleChange.bind(this)} value={this.state.value}>
+        <option value='' disabled>--Actions--</option>
+        <option value='renew'>Renew</option>
+        <option value='return'>Return</option>
+        <option value='lost'>Lost</option>
+      </select>
     );
   }
 }
 
 class LoanRow extends Component {
   render(){
-    const { num, loan } = this.props;
+    const { num, loan, actions } = this.props;
     return (
       <tr>
+        <td><input type='checkbox' /></td>
         <td>{ num }</td>
         <td>{loan.resource.code}</td>
         <td>{loan.resource.title}</td>
         <td>{loan.loan_at}</td>
-        <td>-</td>
+        <td><LoanActions loan={loan.id} onSelect={actions.selectLoanAction}/></td>
       </tr>
     );
   }
@@ -208,12 +287,13 @@ class LoanRow extends Component {
 
 class LoanTable extends Component {
   render(){
-    const { loans } = this.props;
+    const { loans, actions } = this.props;
 
     return (
       <table>
         <thead>
           <tr>
+            <th><input type='checkbox' /></th>
             <th>#</th>
             <th>Identifier</th>
             <th>Title</th>
@@ -225,7 +305,7 @@ class LoanTable extends Component {
           {
             loans.map((obj, i) => {
               return (
-                <LoanRow key={i} loan={obj} num={i+1} />
+                <LoanRow key={i} loan={obj} num={i+1} actions={actions}/>
               );
             })
           }
@@ -237,10 +317,38 @@ class LoanTable extends Component {
 
 
 class CheckoutForm extends Component {
+
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      resource: '',
+    };
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    this.props.onCheckout(this.props.patron, this.state.resource);
+    this.setState({ resource : '' });
+  }
+
+  handleChange(e) {
+    this.setState({
+      resource: e.target.value,
+    });
+  }
+
   render() {
     return (
       <div>
-        Checkout Form
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <label htmlFor="checkout-input">Resource Code:</label>
+          <input
+            type="text"
+            id="checkout-input"
+            value={this.state.resource}
+            onChange={this.handleChange.bind(this)} />
+          <button>Checkout</button>
+        </form>
       </div>
     );
   }
@@ -248,11 +356,11 @@ class CheckoutForm extends Component {
 
 class CirculationPanel extends Component {
   render(){
-    const { circulation, actions } = this.props;
+    const { patron, circulation, actions } = this.props;
     return (
       <div>
-        <CheckoutForm></CheckoutForm>
-        <LoanTable loans={circulation.loans}></LoanTable>
+        <CheckoutForm patron={patron} onCheckout={actions.checkoutResource} />
+        <LoanTable loans={circulation.loans} actions={actions}></LoanTable>
       </div>
     );
   }
@@ -268,8 +376,14 @@ class App extends Component {
         <PatronPanel
           patronProfile={patron.profile}
           onLookup={actions.lookupPatron} /> 
-        <CirculationPanel
-          circulation={patron.circulation} actions={actions} />
+        {
+          patron.profile.id ? (
+          <CirculationPanel
+            patron={patron.profile.id}
+            circulation={patron.circulation}
+            actions={actions} />
+            ) : null
+        }
       </div>
     );
   }
